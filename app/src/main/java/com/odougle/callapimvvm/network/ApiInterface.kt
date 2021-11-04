6f0daae541b8d2124cad2d5d3c8390bb56635c63
Douglas Gomes <douglasgommez@gmail.com>
2021-11-04 20:56:58 -0300

    set ApiInterface.kt as singleton

--- app/src/main/java/com/odougle/callapimvvm/network/ApiInterface.kt
+++ app/src/main/java/com/odougle/callapimvvm/network/ApiInterface.kt
@@ -2,7 +2,9 @@ package com.odougle.callapimvvm.network
 
 import com.odougle.callapimvvm.model.UserResponse
 import retrofit2.http.GET
+import javax.inject.Singleton
 
+@Singleton
 interface ApiInterface {
 
     @GET("todos")
