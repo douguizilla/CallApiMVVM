30df1983cf82eb5f64fe43b3921356ef59744fa8
Douglas Gomes <douglasgommez@gmail.com>
2021-11-04 20:22:36 -0300

    clean unnecessary code

--- app/src/main/java/com/odougle/callapimvvm/MainActivity.kt
+++ app/src/main/java/com/odougle/callapimvvm/MainActivity.kt
@@ -17,22 +17,19 @@ class MainActivity : ComponentActivity() {
             CallApiMVVMTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+
+
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     CallApiMVVMTheme {
-        Greeting("Android")
+
     }
 }
