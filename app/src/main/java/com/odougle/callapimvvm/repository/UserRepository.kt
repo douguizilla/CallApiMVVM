9b83e054926c457247f4c41740b4717201366588
Douglas Gomes <douglasgommez@gmail.com>
2021-11-05 17:52:27 -0300

    created getUserResponse function on UserRepository.kt

--- app/src/main/java/com/odougle/callapimvvm/repository/UserRepository.kt
+++ app/src/main/java/com/odougle/callapimvvm/repository/UserRepository.kt
@@ -1,9 +1,19 @@
 package com.odougle.callapimvvm.repository
 
+import com.odougle.callapimvvm.model.UserResponse
 import com.odougle.callapimvvm.network.ApiInterface
+import com.odougle.callapimvvm.utils.Resource
 import javax.inject.Inject
 
 class UserRepository @Inject constructor(
     private val apiInterface: ApiInterface
 ){
+    suspend fun getUserResponse() : Resource<List<UserResponse>>{
+        val response = try {
+            apiInterface.getUserData()
+        }catch (e: Exception){
+            return Resource.Error("An unkown error occurred: ${e.localizedMessage}")
+        }
+        return Resource.Sucess(response)
+    }
 }
